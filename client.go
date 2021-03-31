aff870f2f31835eaddb144a6614d867947d2c1eb
Alessandro <alessandro.ruggiero03@gmail.com>
2021-03-31 09:42:24 +0200

    doc

--- client.go
+++ client.go
@@ -81,7 +81,7 @@ func (c *Client) writePump() {
 		c.Conn.Close()
 		c.Active = false
 	}()
-	for {
+	for { // receve loop
 		select {
 		case message, ok := <-c.send:
 			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
@@ -96,13 +96,6 @@ func (c *Client) writePump() {
 				return
 			}
 			w.Write(message)
-
-			// Add queued chat messages to the current websocket message.
-			// n := len(c.send)
-			// for i := 0; i < n; i++ {
-			// 	w.Write(newline)
-			// 	w.Write(<-c.send)
-			// }
 
 			if err := w.Close(); err != nil {
 				return
